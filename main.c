05ded1b09a6a612fd334772d0873fc5094940908
CarlosGiovaneNN <carlosgiovane1289@gmail.com>
2024-06-14 19:12:11 -0300

    final commit

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-#define ASCII 128
-#define COUNT 10
+#define ASCII 256
 
 typedef struct huffman{
     char data;
@@ -13,6 +11,7 @@ typedef struct huffman{
     struct huffman *one;    
 } Huffman;
 
+//ADICIONANDO NO VETOR
 void add(char vector[], int qtd[], char c, int *size) {
     
     int flag = 0;
@@ -32,146 +31,243 @@ void add(char vector[], int qtd[], char c, int *size) {
     }
 }
 
-void boobleSort(char vector[], int qtd[], int size) {
+//ORDENANDO VETORES
+void bubbleSort(char vector[], int qtd[], int size) {
 
-    int flag = 0;
-    int i = size - 1;
+    int flag;
 
-    while(i >= 0) {
+    for (int i = 0; i < size - 1; i++) {
 
-        for(int j = 0; j < size; j++) {
+        flag = 0;
 
-            if(qtd[j] > qtd[j + 1]) {
-               
-                int aux = qtd[j]; 
-                char c = vector[j];
+        for (int j = 0; j < size - 1 - i; j++) {
 
-                vector[j] = vector[j + 1];
+            if (qtd[j] > qtd[j + 1] || (qtd[j] == qtd[j + 1] && vector[j] > vector[j + 1])) {
+
+                int aux = qtd[j];
                 qtd[j] = qtd[j + 1];
-
-                vector[j + 1] = c;
                 qtd[j + 1] = aux;
 
-                flag = 1;
-            }
-
-            else if(qtd[j] == qtd[j + 1] && vector[j] > vector[j + 1]) {
-               
-                int aux = qtd[j]; 
                 char c = vector[j];
-
                 vector[j] = vector[j + 1];
-                qtd[j] = qtd[j + 1];
-
                 vector[j + 1] = c;
-                qtd[j + 1] = aux;
 
                 flag = 1;
             }
         }
 
-        if(!flag) break;
-        i--;
+        if (!flag) break;
     }
 }
 
-void createLeafs(char vector[], int size, Huffman data[], int qtd[]) {
+//CRIANDO FOLHAS
+void createLeafs(char vector[], int size, Huffman *array[], int qtd[]) {
 
     for(int i = 0; i < size; i++) {
 
-        data[i].data = vector[i];
-        data[i].oc = qtd[i];
-        data[i].one = NULL;
-        data[i].zero = NULL;
+        Huffman *aux = (Huffman *)malloc(sizeof(Huffman));
+        aux->one = NULL;
+        aux->zero = NULL;
+
+        aux->data = vector[i];
+        aux->oc = qtd[i];
+
+        array[i] = aux;
     }
 }
 
-Huffman *makeHuffman(char vector[], int qtd[], int size) {
+void makeHuffman(Huffman *array[], int size) {
 
-    Huffman *data = (Huffman*)malloc(size * sizeof(Huffman));
+    while (size > 1) {
 
-    createLeafs(vector, size, data, qtd);
-
-    while (size != 1) {
+        //ATRIBUINDO OS VALORES DO NOVO NÓ, ATRAVÉS DE UM AUXILIAR
         Huffman *aux = (Huffman *)malloc(sizeof(Huffman));
+
+        //COLOCANDO O CARACTER DENTRO DO NÓ COMO VAZIO
         aux->data = '\0';
-        aux->oc = qtd[0] + qtd[1];
-        aux->one = &data[1];
-        aux->zero = &data[0];
+        int number = array[0]->oc + array[1]->oc;
+        aux->oc = number;
+        aux->one = array[1];
+        aux->zero = array[0];
 
-        for (int i = 0; i < size - 2; i++) {
-            data[i] = data[i + 2];
-            qtd[i] = qtd[i + 2];
+        //PUXANDO OS ELEMENTOS
+        for (int i = 0; i < size - 2; i++) array[i] = array[i + 2];
+
+        array[size - 2] = aux;
+
+        //ORDENANDO
+        for (int i = size - 2; i > 0 && array[i - 1]->oc > array[i]->oc; i--) {
+
+            Huffman *auxData = array[i];
+            array[i] = array[i - 1];
+            array[i - 1] = auxData;
         }
-
-        data[size - 2] = *aux;
-        qtd[size - 2] = aux->oc;
-        qtd[size - 1] = 0;
-
-        int current = size - 3;
-        
-        while (current >= 0) {
-            if (qtd[current] > aux->oc) {
-                qtd[current + 1] = qtd[current];
-                data[current + 1] = data[current];
-                qtd[current] = aux->data;
-                data[current] = *aux;
-            }
-            current--;
-        }
-
         size--;
     }
-
-    return &data[0];
 }
 
-void print2DUtil(Huffman* root) {
+//RECURCIVIDADE PARA RETORNAR O CODIGO DA LETRA 
+void findCodeLetter(Huffman *root, char *cod, char c, int depth, char *path) {
     
-    if (root == NULL)
+    if(root == NULL) return;
+
+    if (root->data == c) {
+        cod[depth] = '\0';
+        strcpy(path, cod);
         return;
- 
-    print2DUtil(root->one);
+    }
 
-    printf("\n %c", root->data);
+    if (root->zero != NULL) {
+        cod[depth] = '0';
+        findCodeLetter(root->zero, cod, c, depth + 1, path);
+    }
 
-    print2DUtil(root->zero);
+    if (root->one != NULL) {
+        cod[depth] = '1';
+        findCodeLetter(root->one, cod, c, depth + 1, path);
+    }
+
+    return;
 }
- 
-void print2D(Huffman* root) {
-    print2DUtil(root);
+
+//CODIFICAR O ARQUIVO
+void codeFile(FILE *txt, Huffman *root) {
+
+    FILE *code = fopen("codificado.txt", "w");
+
+    if(code == NULL) exit(1);
+
+    char c = fgetc(txt);
+
+    while (!feof(txt)) {
+        if (c != '\n') {
+            char aux[8];
+            char path[8];
+            findCodeLetter(root, aux, c, 0, path);
+            fputs(path, code);
+        }
+        c = fgetc(txt);
+    }
+
+    fclose(code);
 }
 
+//RECURSIVIDADE PARA RETORNAR A LETRA DO CODIGO
+void findCode(FILE *txt, Huffman *root, char *character, int *flag) {
+
+    if(root == NULL || *flag == 1) return;
+    
+    if(root->data != '\0') {
+        *character = root->data;
+        *flag = 1;
+        return;
+    }
+
+    char c = fgetc(txt);
+
+    if(c == '0') findCode(txt, root->zero, character, flag);
+
+    if (c == '1') findCode(txt, root->one, character, flag);
+
+}
+
+//DECODIFICAR ARQUIVO
+void decodeFile(FILE *txt, Huffman *root) {
+
+    FILE *decode = fopen("decodificado.txt", "w");
+
+    if(decode == NULL) exit(1);
+
+    char c = fgetc(txt);
+    char character = '\0';
+
+    while (!feof(txt)) {
+
+        int flag = 0;
+        
+        if(c == '0') findCode(txt, root->zero, &character, &flag);
+
+        else if (c == '1') findCode(txt, root->one, &character, &flag);
+
+        c = fgetc(txt);
+        fputc(character, decode);
+    }
+
+    fclose(decode);
+}
 
 int main() {
 
     FILE *txt = fopen("amostra.txt", "r"); 
-    if (txt == NULL) {
-        printf("Erro ao abrir o arquivo");
-        exit(1);
-    }
+    
+    if (txt == NULL) exit(1);
 
     char c= fgetc(txt);
     char vector[ASCII];
     int qtd[ASCII];
     int size = 0;
 
+    //LER O ARQUIVO
     while (!feof(txt)) {
-        
-      
-
         add(vector, qtd, c, &size);
-
-          c = fgetc(txt);
+        c = fgetc(txt);
     }
 
-    boobleSort(vector, qtd, size);
+    //SE O ARQUIVO ESTIVER EM BRANCO ELE DEIXAO EM BRANCO, MESMO QUE NOS DOIS TXT(codificado.txt E O decodificado.txt) ESTIVESSEM ALGO DENTRO ANTES
+    if(size == 0) {
+        FILE *code = fopen("codificado.txt", "w");
+        FILE *decode = fopen("decodificado.txt", "w");
+        fclose(code);
+        fclose(decode);
+        fclose(txt);
+        exit(1);
+    }
+
+    bubbleSort(vector, qtd, size);
+
+    Huffman *root = (Huffman *)malloc(sizeof(Huffman));
+
+    //CRIARÁ UMA HUFFMAN DE SOMENTE UMA LETRA
+    if(size > 1) {
+
+        Huffman *array[size];
+
+        createLeafs(vector, size, array, qtd);
+        makeHuffman(array, size);
+
+        root = array[0];
+
+    } else {
+
+        //CRIARÁ UMA HUFFMAN DE MAIS LETRAS
+        root->one = NULL;
+        root->oc = qtd[0];
+        root->data = '\0';
+
+        Huffman *aux = (Huffman *)malloc(sizeof(Huffman));
+
+        aux->data = vector[0];
+        aux->oc = qtd[0];
+        aux->one = NULL;
+        aux->zero = NULL;
+
+        root->zero = aux;
+    }
+
+    //RESETA O PONTEIRO DO ARQUIVO
+    rewind(txt);
+    codeFile(txt, root);
+
+    FILE *code = fopen("codificado.txt", "r");
     
-    Huffman *root = makeHuffman(vector, qtd, size);
+    //RESETA O PONTEIRO DO ARQUIVO
+    rewind(code);
+    decodeFile(code, root);
 
-    print2D(root);
-
+    //LIMPA E FECHA TUDO QUE PRECISA
+    free(root);
+    fclose(code);
     fclose(txt);
-
+    
     return 0;
 }
